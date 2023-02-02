bd94e155052a9cdb43e089ccc168b6097003a770
Bestian Tang 大巴 <bestian@gmail.com>
2023-02-02 15:11:04 +0800

    get Prime

--- src/pages/CheckOutPage.vue
+++ src/pages/CheckOutPage.vue
@@ -2,7 +2,9 @@
   <q-page class="row items-center justify-evenly">
     <div>
         <label>CardView</label>
-        <div id="tappay-iframe"></div>
+        <div id="cardview-container"></div>
+
+        <q-btn @click="pay()">支付</q-btn>
     </div>
   </q-page>
 </template>
@@ -37,9 +39,10 @@ export default defineComponent({
         lineHeight: '24px',
         fontWeight: '300',
         errorColor: 'red',
-        placeholderColor: ''
+        placeholderColor: '',
+
     }
-    
+
     // 帶入第三個參數, config.isUsedCcv 為以下兩種參數代表不同意思
     // false 為 CCV 非必填
     // true 為 CCV 必填
@@ -55,13 +58,21 @@ export default defineComponent({
     // endIndex: Int
     // 結束遮蔽卡號位元
     TPDirect.card.setup('#cardview-container', defaultCardViewStyle, { 
-        isUsedCcv: false,
+        isUsedCcv: true,
         // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
         isMaskCreditCardNumber: true,
         maskCreditCardNumberRange: {
             beginIndex: 6, 
             endIndex: 11
         }
+    })
+
+    TPDirect.card.onUpdate(function(update){
+      if (update.canGetPrime) {
+        console.log('good!')
+      }
+
+      /*   ... */
     })
   },
   methods: {
@@ -80,7 +91,30 @@ export default defineComponent({
     },
     buy (i) {
       /// 
+    },
+    pay () {
+      TPDirect.card.getPrime(function(result) {
+          if (result.status !== 0) {
+            console.error('getPrime error')
+            window.alert('請輸入正確的卡號和日期與安全碼')
+            return
+          }
+          var prime = result.card.prime
+          console.log('getPrime success: ' + prime)
+      })
     }
   }
 });
 </script>
+
+<style type="text/css">
+  #cardview-container {
+    color: 'rgb(0,0,0)';
+    fontSize: '15px';
+    lineHeight: '24px';
+    fontWeight: '300';
+    errorColor: 'red';
+    placeholderColor: '';
+    width: 400px;
+  }
+</style>
