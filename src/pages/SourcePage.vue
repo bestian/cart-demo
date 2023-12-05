6a95fb456e20a83d6872a66f6c57c0f7ccad91fe
Bestian Tang <bestian@gmail.com>
2023-12-05 11:52:11 +0800

    Update SourcePage.vue

--- src/pages/SourcePage.vue
+++ src/pages/SourcePage.vue
@@ -3,18 +3,14 @@
     class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
   >
     <div>
-      <div style="font-size: 30vh">本專案採用MIT授權條款</div>
-
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
-
+      <h4>本專案採用MIT授權條款開源</h4>
       <q-btn
         class="q-mt-xl"
         color="white"
         text-color="blue"
         unelevated
-        to="/"
-        icon="github"
-        label="View Source Code"
+        @click="goto_source()"
+        label="Fork me on GitHub"
         no-caps
       />
     </div>
@@ -26,5 +22,10 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'ErrorNotFound',
+  methods: {
+    goto_source() {
+      window.open('https://www.github.com/bestian/cart-demo');
+    },
+  },
 });
 </script>
