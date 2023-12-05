5c713b19781c4e463501537525ff6262baa75882
Bestian Tang <bestian@gmail.com>
2023-12-05 11:28:13 +0800

    debug & 重新指向資料庫

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,36 +1,34 @@
-import { boot } from 'quasar/wrappers'
+import { boot } from 'quasar/wrappers';
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database' 
-import { getAuth } from 'firebase/auth'
-import { VueFire, VueFireAuth } from 'vuefire'
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+import { VueFire, VueFireAuth } from 'vuefire';
 
 // Get a Firestore instance
 export const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyCoY777ID9SQqz7UPyjha9YRycO2Y56bZ8',
-  authDomain: 'payment-demo-14e75.firebaseapp.com',
-  databaseURL: 'https://payment-demo-14e75-default-rtdb.firebaseio.com',
-  projectId: 'payment-demo-14e75',
-  storageBucket: 'payment-demo-14e75.appspot.com',
-  messagingSenderId: '202896970342',
-  appId: '1:202896970342:web:45fcadb0655ffc7c0bd3a7',
-  measurementId: 'G-LSNJMNJM2F'
+  apiKey: 'AIzaSyADI3q1K1tKzsjPstpJoeDS9tRQpeidaOo',
+  authDomain: 'cart-demo-f2340.firebaseapp.com',
+  databaseURL: 'https://cart-demo-f2340-default-rtdb.firebaseio.com',
+  projectId: 'cart-demo-f2340',
+  storageBucket: 'cart-demo-f2340.appspot.com',
+  messagingSenderId: '1055844402423',
+  appId: '1:1055844402423:web:a42ceee1fa2dfb96ddfff2',
+  measurementId: 'G-YZD66T2PR1',
 });
 
-
-export const db = getDatabase(firebaseApp)
-export const auth = getAuth(firebaseApp)
-
+export const db = getDatabase(firebaseApp);
+export const auth = getAuth(firebaseApp);
 
 // 'async' is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ app }) => {  //, router
-  app
-    .use(VueFire, {
-      // imported above but could also just be created here
-      firebaseApp,
-      modules: [
-        // we will see other modules later on
-        VueFireAuth(),
-      ],
-    })
-})
+export default boot(async ({ app }) => {
+  //, router
+  app.use(VueFire, {
+    // imported above but could also just be created here
+    firebaseApp,
+    modules: [
+      // we will see other modules later on
+      VueFireAuth(),
+    ],
+  });
+});
